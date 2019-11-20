48356c8884d5ca02f75550fd8b0c0f52f31348d3
Josh Cormier <31702119+jcorm714@users.noreply.github.com>
2019-11-20 13:03:07 -0500

    Update Classifier.py

--- Classifier.py
+++ Classifier.py
@@ -47,25 +47,33 @@ def getDataFrame(path, classification):
 
 cwd = os.getcwd()
 
-#set 
+#get data from the emails subdirectory in the project
 data = DataFrame({'message': [], 'class': []})
 data = data.append(getDataFrame(os.path.join(cwd, 'emails/spam'), 'spam'))
 data = data.append(getDataFrame(os.path.join(cwd, 'emails/ham'), 'ham'))
 
+#verify that data has been read in
 print(data.head())
- 
+
+#split the data into trainig & testing data
+#usually 80 20 is good split. The majority of the 
+#data is used for training.
 train_data = data.sample(frac =0.8)
 test_data = data.sample(frac = 0.2)
 
+#get a count of each instance of the words in all the emails
 vectorizer = CountVectorizer()
 counts = vectorizer.fit_transform(train_data['message'].values)
 
-
+#create the Multinomial Naive Bayes classifier
+#and train it on the training data.
 classifier = MultinomialNB()
 targets = train_data['class'].values
 classifier.fit(counts, targets)
 
-
+#predict the values of the test data. 
+#Can see how effienct the model is based
+#off wheter the email is spam or ham
 example_counts = vectorizer.transform(test_data['message'].values)
 predictions = classifier.predict(example_counts)
 print(predictions)
